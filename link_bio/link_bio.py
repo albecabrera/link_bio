502cdc173d6ad788b9942736e82f224c99eb0342
albecabrera <albe.cabrera@yahoo.de>
2024-07-16 01:57:43 +0200

    commit -m "change 3"

--- link_bio/link_bio.py
+++ link_bio/link_bio.py
@@ -1,13 +1,15 @@
 import reflex as rx
 from link_bio.components.navbar import navbar
 from link_bio.views.header.header import header
+from link_bio.views.links.links import links
 
 class State(rx.State):
     pass
 def index() -> rx.Component:
    return rx.vstack(
         navbar(),
-        header()
+        header(),
+        links(),
    )
 app = rx.App()
 app.add_page(index)
---
+++ link_bio/views/links/links.py
@@ -0,0 +1,10 @@
+import reflex as rx
+from link_bio.components.link_button import link_button
+
+def links() -> rx.Component:
+  return rx.vstack(
+    link_button("Portfolio", "http://albertocabrera.de"),
+    link_button("Framer", "https://portfoliocabrera.framer.website"),
+    link_button("Discord"),
+    link_button("Instagram"),
+  )
